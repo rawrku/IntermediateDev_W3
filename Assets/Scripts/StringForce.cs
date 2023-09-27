3bafd1c4fdbc718d0de44219a4becad01b5084ba
-AKIRA-\ruari <ruarisroot@icloud.com>
2023-09-26 21:01:39 -0400

    added the rest of the trash and coin objects as well as messing with the force script

--- Assets/Scripts/StringForce.cs
+++ Assets/Scripts/StringForce.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class StringForce : MonoBehaviour
 {
+
+    private float timer;
+
+    private float maxTime = 4f;
+    private float minTime = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = Random.Range(minTime, maxTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -700));
+            timer = Random.Range(minTime, maxTime);
+            Debug.Log("FORCE");
+
+        } else
+        {
+            timer -= Time.deltaTime;
+        }
     }
 }
